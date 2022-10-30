bd00871f5b6d5b96a10fbaf0fb66c901f86cc9e9
Carlos <cnoramb1@my.centennialcollege.ca>
2022-10-29 20:54:49 -0400

    Initial Commit

--- Shopping List App/ViewController.swift
+++ Shopping List App/ViewController.swift
@@ -14,47 +14,60 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func TxtListName(_ sender: UITextField) {
+    }
     
-    //Label Quantity
-    @IBOutlet weak var LblSugar: UILabel!
-    @IBOutlet weak var LblSalt: UILabel!
-    @IBOutlet weak var LblRice: UILabel!
-    @IBOutlet weak var LblMilk: UILabel!
-    @IBOutlet weak var LblCheese: UILabel!
-    @IBOutlet weak var LblEggs: UILabel!
-    @IBOutlet weak var LblOil: UILabel!
+    //Text Field Quantity
+    
+    
+    @IBOutlet weak var TxtSugar: UITextField!
+    @IBOutlet weak var TxtSalt: UITextField!
+    @IBOutlet weak var TxtRice: UITextField!
+    @IBOutlet weak var TxtMilk: UITextField!
+    @IBOutlet weak var TxtCheese: UITextField!
+    @IBOutlet weak var TxtEggs: UITextField!
+    @IBOutlet weak var TxtOil: UITextField!
+    
+    
     
     //Quantity of items
     @IBAction func Sugar_Stepper(_ sender: UIStepper) {
         
-        LblSugar.text = String (Int(sender.value))
+        self.TxtSugar.text = Int(sender.value).description
         
     }
     
     @IBAction func Salt_Stepper(_ sender: UIStepper) {
+        
+        
     }
     
     @IBAction func Rice_Stepper(_ sender: UIStepper) {
+        
+        
     }
     
     @IBAction func Milk_Stepper(_ sender: UIStepper) {
+        
+        
     }
     
     
     @IBAction func Cheese_Stepper(_ sender: UIStepper) {
+        
+        
     }
     
     
     @IBAction func Eggs_Stepper(_ sender: UIStepper) {
+        
+        
     }
     
     @IBAction func Oil_Stepper(_ sender: UIStepper) {
+        
+        
     }
-    
-    
-    
-    
-    
-    
+
 }
 
